fbeaac2edda1f91e5f18b3b927376af895879457
eaghayev <eaghayevv@gmail.com>
2023-04-18 17:51:45 +0400

    project added

---
+++ Pustok/Pustok/wwwroot/assets/js/main.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    $(document).on("click", '.open-book-model', function (e) {
+
+        e.preventDefault();
+        var url = $(this).attr("href");
+
+        fetch(url)
+            .then(response => response.text())
+            .then(modalHtml => {
+                $("#quickModal .modal-dialog").html(modalHtml)
+            });
+      
+
+        $("#quickModal").modal("show")
+    })
+
+    $(document).on("click", ".add-basket", function (e) {
+        e.preventDefault();
+
+        var url = $(this).attr("href");
+        let basketNumber = $('.text-number')[0];
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    alert("Xeta bas verdi")
+                    return
+                }
+                else return response.text()
+            })
+            .then(html => $("#basket-cart").html(html))
+
+    })
+})
